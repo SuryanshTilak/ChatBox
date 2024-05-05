0119c9d5f1bdb770c8432f78e692649152c5fa29
Suryansh Tilak <suryansh.tilak@gmail.com>
2024-05-05 11:48:53 +0530

    frontend complete

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,9 +1,26 @@
 import User from "../models/user.model.js";
 
 const getUsersForSidebar = async(req,res) =>{
-    const users= await User.find()
+    try{
+        const LoggedUserId= req.user._id
 
-    res.status(200).send(users)
+        const FilteredUsers= await User.find(
+            { 
+                _id : { $ne : LoggedUserId}
+            }
+        ).select("-password")
+
+        res.status(200).send(FilteredUsers)
+    }
+    catch(error)
+    {
+        console.error("Error in getUserForSidebar : ",error.message)
+        res.status(500).json(
+            {
+                error : "Internal server error"
+            }
+        )
+    }
 }
 
 export default getUsersForSidebar
