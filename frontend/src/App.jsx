16930fb41845a43ad869c7bce9f0d36cab4eaaf6
Suryansh Tilak <suryansh.tilak@gmail.com>
2024-05-13 19:35:31 +0530

    frontend updated 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css"
-import Login from "./pages/login/Login.jsx";
-import  SignUp from "./pages/signup/SignUp.jsx";
+// import Login from "./pages/login/Login.jsx";
+// import  SignUp from "./pages/signup/SignUp.jsx";
 import Home from "./pages/home/Home.jsx"
 import { Route, Routes } from "react-router-dom";
 
@@ -10,8 +10,8 @@ export default function App() {
     <div className="p-4 h-screen flex items-center justify-center ">
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
-        <Route path='/login' element={<Login/>}/>
+        {/* <Route path='/signup' element={<SignUp/>}/> */}
+        {/* <Route path='/login' element={<Login/>}/> */}
       </Routes >
     </div>
   );
