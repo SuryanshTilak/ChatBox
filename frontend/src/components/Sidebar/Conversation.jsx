67c55fa9ee49b734885ed7e9f5caa44d6b4f27c8
Suryansh Tilak <suryansh.tilak@gmail.com>
2024-07-24 01:27:40 +0530

    conversation 1

--- frontend/src/components/Sidebar/Conversation.jsx
+++ frontend/src/components/Sidebar/Conversation.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Conversation = () => {
+const Conversation = ({conversation,lastIdx,emoji}) => {
   return (
     <>
         <div className='flex gap-2 items-center
@@ -8,20 +8,21 @@ const Conversation = () => {
 
             <div className='avatar online'>
                 <div className="w-12 rounded-full">
-                    <img src="" alt="user avatar" />
+                    <img src={conversation.profilePic} alt="user avatar" />
                 </div>
             </div>
 
             <div className='flex flex-col flex-1'>
                 <div className='flex gap-3 justify-between'>
-                    <p className='font-bold text-gray-200'>Om</p>
-                    <span className='text-xl'> </span>
+                    <p className='font-bold text-gray-200'>{conversation.fullName}</p>
+                    <span className='text-xl'>{emoji}</span>
                 </div>
             </div>   
 
         </div>
 
-        <div className='divider my-0 py-0 h-1'></div>
+        {!lastIdx &&  <div className='divider my-0 py-0 h-1'></div>}
+       
     </>
   )
 }
