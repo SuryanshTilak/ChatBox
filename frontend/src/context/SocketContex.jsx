132d29a99d7c601d3217dad01add2c3790110793
Suryansh Tilak <suryansh.tilak@gmail.com>
2024-08-27 04:45:48 +0530

    render link updated at SocketContext

--- frontend/src/context/SocketContex.jsx
+++ frontend/src/context/SocketContex.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children}) =>{
 
     useEffect(()=>{
         if(authUser){
-            const socket = io("http://localhost:5000",{
+            const socket = io("https://chatbox-p0kd.onrender.com",{
                 query:{
                     userId : authUser._id
                 }
