0c3956b4a45c8c3315fb4b2995905a56f0ce9c03
Suryansh Tilak <suryansh.tilak@gmail.com>
2024-08-27 03:57:29 +0530

    frontend connected to backend completed

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,17 @@ import userRoutes from './routes/user.routes.js'
 import connectToMongoDB from "./db/mongoDb.js"
 //cors nhi hai original me
 import cors from 'cors'
+import path from "path"
+
 
 //-------delete after socket.js---------
 //const app=express()
 dotenv.config()// to use .env file
 
 const PORT = process.env.PORT || 5000
+
+//gives absolute path to root folder
+const __dirname = path.resolve()
 
 app.use(express.json()) 
 //to parse the incoming request with JSON payloads (from req.body)
@@ -35,6 +40,13 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
 
+//static dist file
+app.use( express.static(path.join(__dirname, "frontend/dist") ) )
+
+//any routes other than upper 3 will be directed here
+app.get("*" , (req,res) => {
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen(PORT,()=>{
     connectToMongoDB()
